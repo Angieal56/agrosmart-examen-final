f3b451af4631fa64cbe4b06d00bd1749dc096e2a
Angieal56 <analvarado1@espe.edu.ec>
2026-07-31 20:54:07 -0500

    fix: restaurar todos los paquetes y clases del proyecto en main

--- src/test/java/ec/edu/espe/agrosmart/AgrosmartApplicationTests.java
+++ src/test/java/ec/edu/espe/agrosmart/AgrosmartApplicationTests.java
@@ -2,13 +2,12 @@ package ec.edu.espe.agrosmart;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("prod")
 class AgrosmartApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+
 }
